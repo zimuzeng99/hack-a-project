f61d65a2a5ce8a32172631d433be9cf99230cae5
Zimu Zeng <zimu.zeng@gmail.com>
2019-03-09 17:11:07 +0000

    Reverted question mark change in app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def addQuestion(groupID, userID):
 # this allows additional resources like images and CSS files to be provided to the browser upon request
 @app.route("/<path:path>")
 def catchAll(path):
-    return send_file(path[ : path.find("?")])
+    return send_file(path)
 
 if __name__ == "__main__":
     app.run()
